444e9964ee1379b7d203626ded2019462bf16e91
Kiet Pham <kpham@axon.com>
2026-04-14 07:09:33 -0700

    allow debug logger to write log to file

--- src/debug_logger.h
+++ src/debug_logger.h
@@ -2,6 +2,7 @@
 #define KSH_DEBUG_LOGGER_H
 
 #include <iostream>
+#include <fstream>
 
 class DebugLogger {
   public:
@@ -11,16 +12,36 @@ class DebugLogger {
     }
 
     static void configure(bool enabled) { get().mEnabled = enabled; }
+    /**
+     * Set the output file for the debug logger. Note that after calling this function,
+     * the output will be written to the file instead of the console. If the file is not
+     * opened successfully, the output will be written to the console instead.
 
+     * @param path: The path to the output file.
+     */
+    static void set_output_file(const std::string &path) {
+        get().mOutputFileStream.open(path);
+        if (!get().mOutputFileStream.is_open()) {
+            std::cerr << "Failed to open output file " << path << std::endl;
+        }
+    }
     template <typename... Args>
     static void print(Args &&...args) {
         if (!get().mEnabled) {
             return;
         }
 #ifndef TAG
-        (std::cout << ... << std::forward<Args>(args)) << std::endl;
+        if (get().mOutputFileStream.is_open()) {
+            (get().mOutputFileStream << ... << std::forward<Args>(args)) << std::endl;
+        } else {
+            (std::cout << ... << std::forward<Args>(args)) << std::endl;
+        }
 #else
-        (std::cout << "[" << TAG << "] " << ... << std::forward<Args>(args)) << std::endl;
+        if (get().mOutputFileStream.is_open()) {
+            (get().mOutputFileStream << "[" << TAG << "] " << ... << std::forward<Args>(args)) << std::endl;
+        } else {
+            (std::cout << "[" << TAG << "] " << ... << std::forward<Args>(args)) << std::endl;
+        }
 #endif
     }
 
@@ -29,7 +50,19 @@ class DebugLogger {
         if (!get().mEnabled) {
             return;
         }
-        (std::cerr << ... << std::forward<Args>(args)) << std::endl;
+#ifndef TAG
+        if (get().mOutputFileStream.is_open()) {
+            (get().mOutputFileStream << ... << std::forward<Args>(args)) << std::endl;
+        } else {
+            (std::cout << ... << std::forward<Args>(args)) << std::endl;
+        }
+#else
+        if (get().mOutputFileStream.is_open()) {
+            (get().mOutputFileStream << "[" << TAG << "] " << ... << std::forward<Args>(args)) << std::endl;
+        } else {
+            (std::cout << "[" << TAG << "] " << ... << std::forward<Args>(args)) << std::endl;
+        }
+#endif
     }
 
   private:
@@ -37,6 +70,7 @@ class DebugLogger {
     DebugLogger(const DebugLogger &) = delete;
     DebugLogger &operator=(const DebugLogger &) = delete;
     bool mEnabled = true;
+    std::ofstream mOutputFileStream;
 };
 
 #endif // KSH_DEBUG_LOGGER_H
